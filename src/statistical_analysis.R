d9cd25dd28149a9258e45d7deb5196d2494a33a8
Niklas Christoffer Petersen <niklascp@gmail.com>
2017-06-27 21:58:53 +0200

    Wörk, wörk, wörk

--- src/statistical_analysis.R
+++ src/statistical_analysis.R
@@ -4,8 +4,35 @@ library(ggplot2)         # Beautiful plots
 library(ggfortify)       # Plots of PCA
 library(tikzDevice)      # Beautiful plots in tex/tikz
 
+library(xtable)          # Beautiful tables in tex
+
 source('traveldemand.R')
 source('weather.R')
+
+options(xtable.floating = FALSE)
+options(xtable.comment = FALSE)
+options(xtable.timestamp = "")
+options(xtable.hline.after = 0)
+
+ggscreeplot <- function(pcobj, type = c('pev', 'cev')) 
+{
+  type <- match.arg(type)
+  d <- pcobj$sdev^2
+  yvar <- switch(type, 
+                 pev = d / sum(d), 
+                 cev = cumsum(d) / sum(d))
+  
+  yvar.lab <- switch(type,
+                     pev = 'Proportion of explained variance',
+                     cev = 'Cumulative proportion of explained variance')
+  
+  df <- data.frame(PC = 1:length(d), yvar = yvar)
+  
+  ggplot(data = df, aes(x = PC, y = yvar)) + 
+    xlab('Principal components') + ylab(yvar.lab) +
+    geom_bar(stat = 'identity')
+    #geom_point() + geom_path()
+}
 
 # -----------------
 # GET PREPARED DATA
@@ -22,9 +49,12 @@ weather_hour <- weather[,list(
   TemperatureC = mean(TemperatureC, na.rm = TRUE),
   WindSpeedKmH = max(WindSpeedKmH, na.rm = TRUE),
   Precipitationmm = max(Precipitationmm),
+  Humidity = max(Humidity),
   Rain = max(Rain),
   Clear = max(Clear)
   ), by = DateTime]
+
+#traveldemand <- traveldemand[(DayOfWeek == 'Mon' | DayOfWeek == 'Tue' | DayOfWeek == 'Wed' | DayOfWeek == 'Thu' | DayOfWeek == 'Fri') & (Hour == '7' | Hour == '8' | Hour == '9')]
 
 fit <- lm(
   CheckInCount^(1/4) ~ DayOfWeek + Hour + DayOfWeek:Hour + Day,
@@ -39,16 +69,131 @@ dev.off()
 
 drop1(fit, test = 'F')
 
-sqrt(mean(traveldemand$Error^2))
-mean(abs(traveldemand$Error / traveldemand$CheckInCount))
-
 traveldemand$Pred <- predict(fit)^4
-#traveldemand$PredUpr <- (predict(fit, interval = 'pred')^4)[, 2]
-#traveldemand$PredLwr <- (predict(fit, interval = 'pred')^4)[, 3]
 traveldemand$Error <- traveldemand$CheckInCount - traveldemand$Pred
-#traveldemand$ErrorUpr <- traveldemand$PredLwr - traveldemand$Pred
-#traveldemand$ErrorLwr <- traveldemand$PredUpr - traveldemand$Pred
+
+eval_initial <- traveldemand[,
+                             .(
+                               RMSE = sqrt(mean(Error^2)),
+                               MAPE = mean(abs(Error / CheckInCount)) * 100
+                             ),
+                             by = .(DayType, Peek)]
+eval_initial_overall <- traveldemand[,
+                                     .(
+                                       DayType = 'Overall',
+                                       Peek = '',
+                                       RMSE = sqrt(mean(Error^2)),
+                                       MAPE = mean(abs(Error / CheckInCount)) * 100
+                                     )]
+# Export evaluation table
+eval_initial_tab <- rbind(eval_initial, eval_initial_overall, fill = T)
+names(eval_initial_tab) <- c('dt', 'peek', 'RMSE', 'MAPE (%)')
+print(xtable(eval_initial_tab),
+      type = "latex",
+      file = "../tables/model_all_eval.tex",
+      hline.after = c(0,0:(nrow(eval_initial_tab)-1),nrow(eval_initial_tab)-1),
+      include.rownames = FALSE)
+
+# M_np
+model_np <- lm(
+  CheckInCount^(1/4) ~ DayOfWeek + Hour + DayOfWeek:Hour + Day,
+  data = traveldemand[Peek == 'NO'])
+summary(model_np)
+
+pdf("../plots/model_np_assumptions.pdf", width=9, height=12)
+par(mfrow=c(4, 1))
+plot(model_np, which=1:4)
+par(mfrow=c(1, 1))
+dev.off()
+
+drop1(model_wd_np, test = 'F')
+
+traveldemand[Peek == 'NO', Pred := predict(model_np)^4]
+
+# M_mp
+model_mp <- lm(
+  CheckInCount^(1/4) ~ DayOfWeek + Hour + Day,
+  data = traveldemand[Peek == 'MORNING'])
+summary(model_mp)
+
+pdf("../plots/model_mp_assumptions.pdf", width=9, height=12)
+par(mfrow=c(4, 1))
+plot(model_mp, which=1:4)
+par(mfrow=c(1, 1))
+dev.off()
+
+drop1(model_mp, test = 'F')
+
+traveldemand[Peek == 'MORNING', Pred := (predict(model_mp)^4)]
+
+# M_ap
+model_ap <- lm(
+  CheckInCount^(1/4) ~ DayOfWeek + Hour + DayOfWeek:Hour + Day,
+  data = traveldemand[Peek == 'AFTERNOON'])
+summary(model_ap)
+
+pdf("../plots/model_ap_assumptions.pdf", width=9, height=12)
+par(mfrow=c(4, 1))
+plot(model_ap, which=1:4)
+par(mfrow=c(1, 1))
+dev.off()
+
+drop1(model_ap, test = 'F')
+
+traveldemand[Peek == 'AFTERNOON', Pred := predict(model_wd_ap)^4]
+
+# Recalculate error
+traveldemand$Error <- traveldemand$CheckInCount - traveldemand$Pred
 traveldemand$ErrorPct <- traveldemand$Error / traveldemand$Pred
+
+eval_independent <- traveldemand[,
+                                 .(
+                                   RMSE = sqrt(mean(Error^2)),
+                                   MAPE = mean(abs(Error / CheckInCount)) * 100
+                                 ),
+                                 by = .(DayType, Peek)]
+eval_independent_overall <- traveldemand[,
+                                     .(
+                                       DayType = 'Overall',
+                                       Peek = '',
+                                       RMSE = sqrt(mean(Error^2)),
+                                       MAPE = mean(abs(Error / CheckInCount)) * 100
+                                     )]
+# Export evaluation table
+eval_independent_tab <- rbind(eval_independent, eval_independent_overall, fill = T)
+names(eval_independent_tab) <- c('dt', 'peek', 'RMSE', 'MAPE (%)')
+print(xtable(eval_independent_tab),
+      type = "latex",
+      file = "../tables/model_independent_eval.tex",
+      hline.after = c(0,0:(nrow(eval_initial_tab)-1),nrow(eval_initial_tab)-1),
+      include.rownames = FALSE)
+
+shapiro.test(traveldemand[Peek == 'MORNING']$Error);
+
+
+
+#models <- traveldemand[,
+#                       .(fit = list(lm(CheckInCount^(1/4) ~ DayOfWeek + Hour + DayOfWeek:Hour + Day, data = .SD))),
+#                       by = list(DayType, Peek)]
+#setkeyv(models, c('DayType', 'Peek'))
+
+#fill_preds <- function(dt, peek, model) {
+#  traveldemand[DayType == dt & Peek == peek, Pred := predict(model)^4]
+#}
+
+#models[, fill_preds(DayType, Peek, fit), by = .EACHI]
+
+traveldemand[,
+             Pred := predict(models[list(DayType, Peek)]$fit, data = .SD),
+             by = list(DayType, Peek)]
+
+lapply(models, function (x) { predict(x) })
+
+# Ensure tables export path
+dir.create('../tables/', showWarnings = FALSE)
+
+
+
 
 travelcard_week <- traveldemand[('2016-10-03' <= Date) & (Date <= '2016-10-09')]
 
@@ -79,7 +224,7 @@ traveldemand[ErrorPct > quantiles[2], ErrorPctAdj := quantiles[2]]
 summary(traveldemand$ErrorPctAdj)
 
 data <- merge(
-  traveldemand[, list(DateTime, Hour, DayOfWeek, ErrorPctAdj)], 
+  traveldemand[, list(DateTime, Hour, DayOfWeek, ErrorPct, ErrorPctAdj)], 
   weather_hour, 
   by = "DateTime")
 
@@ -87,7 +232,7 @@ ggplot(data, aes(TemperatureC, ErrorPctAdj)) +
   geom_point() +
   geom_smooth()
 
-ggplot(data, aes(Clear, ErrorPctAdj, group = Clear)) +
+ggplot(data, aes(factor(Clear, labels = c('No', 'Yes')), ErrorPctAdj, group = Clear)) +
   geom_boxplot(notch = T)
 
 
@@ -110,43 +255,42 @@ data[Rain > 0, AnyRain := 'Yes']
 ggplot(data, aes(AnyRain, ErrorPctAdj)) + 
   geom_boxplot(notch = TRUE)
 
-fit_anyrain <- lm(ErrorPct ~ AnyRain, data = data)
+fit_anyrain <- lm(ErrorPctAdj ~ AnyRain, data = data)
 anova(fit_anyrain)
 
 quantiles_inner <- quantile(data$ErrorPctAdj, c(.25, .75))
 
 data3 <- data[(ErrorPctAdj < quantiles_inner[1]) | (quantiles_inner[2] < ErrorPctAdj)]
 
-data2 <- data3[,!c("DateTime", "Hour", "DayOfWeek", "Clear", "AnyRain", "ErrorPctAdj"), with = FALSE]
+data2 <- data3[,!c("DateTime", "Hour", "DayOfWeek", "AnyRain", "ErrorPctAdj"), with = FALSE]
 data2 <- scale(data2)
 
 pca <- stats::prcomp(data2)
+plot(pca)
+
+pca$rotation
 
 autoplot(pca,
          data = data.frame(ErrorPct = data3[, ErrorPctAdj]),
          colour = 'ErrorPct',
+         #frame = TRUE,
+         #frame.colour = 'ErrorPct',
+         #frame.type = 't',
          loadings.label  = TRUE,
          loadings.label.size = 3,
          loadings = TRUE) + 
   scale_colour_gradient2() +
   ggsave('../plots/loadings.pdf', width = 10, height = 5)
 
+ggscreeplot(pca)
+
+
+
 summary(data$ErrorPct)
 
-fiterr <- lm(ErrorPctAdj ~ WindSpeedKmH + Rain + TemperatureC + Clear, data = data)
-
+fiterr <- lm(ErrorPct ~ WindSpeedKmH + Rain + TemperatureC + Clear, data = data)
+plot(fiterr)
 summary(fiterr)
 
 par(mfrow=c(2, 2))
 plot(fiterr, which=1:4)
-
-autoplot(cluster::clara(data2, k = 2))
-
-library(broom)
-y <-rnorm(10)
-x <-1:10
-mod <- lm(y ~ x)
-df <- augment(mod)
-
-ggplot(df, aes(x = .fitted, y = .resid)) + geom_point()
-
--- src/traveldemand.R
+++ src/traveldemand.R
@@ -33,17 +33,28 @@ write_example_traveldemand <- function(traveldemand)
 # -----------------------
 
 prep_traveldemand <- function() {
-  travelcard <- as.data.table(read_delim("../data/travelcard_2.csv", ";"))
-  travelcard <- travelcard[!(('2016-12-23' <= Date) & (Date <= '2017-01-01'))]
-  travelcard$DayOfWeek <- factor(travelcard$DayOfWeek, 
+  traveldemand <- as.data.table(read_delim("../data/travelcard_2.csv", ";"))
+  traveldemand <- traveldemand[!(('2016-12-23' <= Date) & (Date <= '2017-01-01'))]
+  traveldemand$DayOfWeek <- factor(traveldemand$DayOfWeek, 
                                  labels = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"))
-  travelcard$Hour <- factor(travelcard$Hour)
-  travelcard
+  traveldemand$Hour <- factor(traveldemand$Hour)
+  traveldemand
 }
 
 transform_traveldemand <- function(traveldemand)
 {
   traveldemand$Day <- as.integer(traveldemand$Date - min(traveldemand$Date))
   traveldemand$DateTime <- as.POSIXct(format(traveldemand$Date)) + (as.integer(traveldemand$Hour) - 1) * 60 * 60
+  
+  traveldemand[, DayType := 'WEEKDAY']
+  traveldemand[DayOfWeek == 'Sat', DayType := 'WEEKEND']
+  traveldemand[DayOfWeek == 'Sun', DayType := 'WEEKEND']
+  traveldemand$DayType = factor(traveldemand$DayType)
+  
+  traveldemand[, Peek := 'NO']
+  traveldemand[DayType == 'WEEKDAY' & Hour %in% c('7', '8'), Peek := 'MORNING']
+  traveldemand[DayType == 'WEEKDAY' & Hour %in% c('15', '16', '17'), Peek := 'AFTERNOON']
+  traveldemand$Peek = factor(traveldemand$Peek)
+  
   traveldemand
 }
